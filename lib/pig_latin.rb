de122f3c643822195b92c26823afaf3d3ca1f1e4
Mark Ellis <ellismarkf@gmail.com>
2014-06-13 12:56:10 -0500

    PigLatin moves consonants to the end of a word

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -4,8 +4,10 @@ module PigLatin
   def self.translate(word)
     if begins_with_vowel?(word)
       word + "way"
+    elsif begins_with_y?(word)
+      translate_y_word(word)
     else
-      false
+      translate_others(word)
     end
   end
 
@@ -16,6 +18,34 @@ module PigLatin
     else
       false
     end
+  end
+
+  def self.transpose_cons(word)
+    letters = word.split('')
+    if /\A[^aeiouy]+/.match(letters.first)
+      true
+    else
+      false
+    end
+  end
+
+  def self.begins_with_y?(word)
+    letters = word.split('')
+    if /[y]/.match(letters.first)
+      true
+    else
+      false
+    end
+  end
+
+  def self.translate_y_word(word)
+    y_word = /(y)(.+)/.match(word)
+    y_word[2] + y_word[1] + "ay"
+  end
+
+  def self.translate_others(word)
+    new_word = /(\A[^aeiouy]+)(.+)/.match(word)
+    new_word[2] + new_word[1] + "ay"
   end
 
 end
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -4,10 +4,17 @@ describe PigLatin do
   before(:each) do
     @test_cons = PigLatin.translate("glove")
     @test_vowel = PigLatin.translate("egg")
-    @test3 = PigLatin.translate("rhythm")
+    @test_y1 = PigLatin.translate("rhythm")
+    @test_y2 = PigLatin.translate("yellow")
   end
+
   it 'knows when a word begins with a vowel' do
-    expect(@test_cons).to eq(false)
     expect(@test_vowel).to eq("eggway")
+  end
+
+  it 'moves consonants to the end of a word' do
+    expect(@test_cons).to eq("oveglay")
+    expect(@test_y1).to eq("ythmrhay")
+    expect(@test_y2).to eq("ellowyay")
   end
 end
